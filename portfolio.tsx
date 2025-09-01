66500cfc203e00ed7edfeaec9103e0f41b267e45
Manuel <manuelalejandro9604@gmail.com>
2025-09-01 13:44:21 -0400

    default theme dark

--- portfolio.tsx
+++ portfolio.tsx
@@ -12,7 +12,7 @@ import { Footer } from "@/components/footer"
 
 export default function Portfolio() {
   return (
-    <ThemeProvider defaultTheme="light">
+    <ThemeProvider defaultTheme="dark">
       <LanguageProvider defaultLanguage="es">
         <div className="min-h-screen bg-background text-foreground">
           <Header />
